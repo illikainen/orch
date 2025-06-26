9229f7ea546b6e3d75506af5bcfda67e5d98cfd9
Hans Jerry Illikainen <hji@dyntopia.com>
2025-06-26 15:11:11 +0000

    task: add cert_unbundle

---
+++ src/tasks/cert_unbundle/task.go
@@ -0,0 +1,20 @@
+//lint:ignore ST1003 readability
+package cert_unbundle // revive:disable-line:var-naming
+
+import (
+	"os"
+
+	"github.com/illikainen/orch/src/configs"
+
+	"github.com/zclconf/go-cty/cty"
+)
+
+type Task struct {
+	Condition bool            `json:"condition" hcl:"condition,optional"`
+	Src       string          `json:"src"       hcl:"src"`
+	Dst       string          `json:"dst"       hcl:"dst"`
+	FileMode  os.FileMode     `json:"file_mode" hcl:"file_mode,optional"`
+	DirMode   os.FileMode     `json:"dir_mode"  hcl:"dir_mode,optional"`
+	Config    *configs.Config `json:"config"    hcl:"-"`
+	value     cty.Value
+}
