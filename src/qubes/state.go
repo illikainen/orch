476d766b74a38daeb02212815e961dd6924a9fa2
Hans Jerry Illikainen <hji@dyntopia.com>
2025-07-02 07:54:41 +0000

    qubes: add Check()

--- src/qubes/state.go
+++ src/qubes/state.go
@@ -5,6 +5,18 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func Check(name string) (bool, error) {
+	p, err := process.Exec(&process.ExecOptions{
+		Command:         []string{"qvm-check", "--", name},
+		IgnoreExitError: true,
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return p.ExitCode == 0, nil
+}
+
 func Start(name string) error {
 	log.Debugf("qubes: starting %s", name)
 	_, err := process.Exec(&process.ExecOptions{
