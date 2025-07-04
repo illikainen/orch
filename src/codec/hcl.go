0ba8908332b97fc3ffae099fbe3b5b508b8c3511
Hans Jerry Illikainen <hji@dyntopia.com>
2025-07-04 19:07:20 +0000

    codec: hcl: support more types in GenerateObjectSpec() and improve tag parsing

--- src/codec/hcl.go
+++ src/codec/hcl.go
@@ -25,8 +25,8 @@ func GenerateObjectSpec(v any) (*hcldec.ObjectSpec, error) {
 			continue
 		}
 
-		name, optional, dynamic := parseFieldTag(field)
-		if name == "-" {
+		tags := parseFieldTags(field)
+		if tags.Name == "-" {
 			continue
 		}
 
@@ -36,22 +36,28 @@ func GenerateObjectSpec(v any) (*hcldec.ObjectSpec, error) {
 		}
 
 		if kind == reflect.Bool {
-			spec[name] = &hcldec.AttrSpec{
-				Name:     name,
-				Type:     fn.Ternary(dynamic, cty.DynamicPseudoType, cty.Bool),
-				Required: !optional,
+			spec[tags.Name] = &hcldec.AttrSpec{
+				Name:     tags.Name,
+				Type:     fn.Ternary(tags.Type != cty.NilType, tags.Type, cty.Bool),
+				Required: tags.Required,
 			}
-		} else if kind == reflect.Int64 {
-			spec[name] = &hcldec.AttrSpec{
-				Name:     name,
-				Type:     fn.Ternary(dynamic, cty.DynamicPseudoType, cty.Number),
-				Required: !optional,
+		} else if kind == reflect.Uint32 || kind == reflect.Int || kind == reflect.Int64 {
+			spec[tags.Name] = &hcldec.AttrSpec{
+				Name:     tags.Name,
+				Type:     fn.Ternary(tags.Type != cty.NilType, tags.Type, cty.Number),
+				Required: tags.Required,
 			}
 		} else if kind == reflect.String {
-			spec[name] = &hcldec.AttrSpec{
-				Name:     name,
-				Type:     fn.Ternary(dynamic, cty.DynamicPseudoType, cty.String),
-				Required: !optional,
+			spec[tags.Name] = &hcldec.AttrSpec{
+				Name:     tags.Name,
+				Type:     fn.Ternary(tags.Type != cty.NilType, tags.Type, cty.String),
+				Required: tags.Required,
+			}
+		} else if kind == reflect.Slice && field.Type.Elem().Kind() == reflect.String {
+			spec[tags.Name] = &hcldec.AttrSpec{
+				Name:     tags.Name,
+				Type:     fn.Ternary(tags.Type != cty.NilType, tags.Type, cty.List(cty.String)),
+				Required: tags.Required,
 			}
 		} else if kind == reflect.Struct && field.Anonymous {
 			inner, err := GenerateObjectSpec(reflect.New(field.Type).Interface())
@@ -63,40 +69,64 @@ func GenerateObjectSpec(v any) (*hcldec.ObjectSpec, error) {
 				spec[key] = value
 			}
 		} else if kind == reflect.Struct {
-			// NOTE: untested
-			inner, err := GenerateObjectSpec(reflect.New(field.Type).Interface())
-			if err != nil {
-				return nil, err
-			}
+			// This is a workaround for wrappers like codec/regexp.go,
+			// which should be a string attribute rather than a block.
+			if tags.Type != cty.NilType {
+				spec[tags.Name] = &hcldec.AttrSpec{
+					Name:     tags.Name,
+					Type:     tags.Type,
+					Required: tags.Required,
+				}
+			} else {
+				inner, err := GenerateObjectSpec(reflect.New(field.Type).Interface())
+				if err != nil {
+					return nil, err
+				}
 
-			spec[name] = &hcldec.BlockSpec{
-				TypeName: name,
-				Nested:   inner,
-				Required: !optional,
+				spec[tags.Name] = &hcldec.BlockSpec{
+					TypeName: tags.Name,
+					Nested:   inner,
+					Required: tags.Required,
+				}
 			}
 		} else {
-			return nil, errors.Errorf("%s: unsupported type: %s", name, kind)
+			return nil, errors.Errorf("%s: unsupported type: %s", tags.Name, kind)
 		}
 	}
 
 	return &spec, nil
 }
 
-func parseFieldTag(field reflect.StructField) (name string, optional bool, dynamic bool) {
-	value := field.Tag.Get("orch")
-	dynamic = seq.Contains(strings.Split(value, ","), "dynamic")
+type tags struct {
+	Name     string
+	Required bool
+	Type     cty.Type
+}
 
-	value = field.Tag.Get("hcl")
-	if value != "" {
-		elts := strings.Split(value, ",")
-		return elts[0], seq.Contains(elts[1:], "optional"), dynamic
+func parseFieldTags(field reflect.StructField) *tags {
+	orch := strings.Split(field.Tag.Get("orch"), ",")
+	hc := strings.Split(field.Tag.Get("hcl"), ",")
+	js := strings.Split(field.Tag.Get("json"), ",")
+
+	t := &tags{}
+
+	if hc[0] != "" {
+		t.Name = hc[0]
+	} else if js[0] != "" {
+		t.Name = js[0]
+	} else {
+		t.Name = strings.ToLower(field.Name)
 	}
 
-	value = field.Tag.Get("json")
-	if value != "" {
-		elts := strings.Split(value, ",")
-		return elts[0], seq.Contains(elts[1:], "omitempty"), dynamic
+	if !seq.Contains(hc[1:], "optional") && !seq.Contains(js[1:], "omitempty") {
+		t.Required = true
 	}
 
-	return strings.ToLower(field.Name), false, dynamic
+	if seq.Contains(orch, "dynamic") {
+		t.Type = cty.DynamicPseudoType
+	} else if seq.Contains(orch, "string") {
+		t.Type = cty.String
+	}
+
+	return t
 }
