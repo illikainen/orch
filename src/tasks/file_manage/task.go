2bdcc9684625e4de92f53984d2a235039745c040
Hans Jerry Illikainen <hji@dyntopia.com>
2025-07-05 05:23:26 +0000

    tasks: file_manage: improve struct tags

--- src/tasks/file_manage/task.go
+++ src/tasks/file_manage/task.go
@@ -10,12 +10,12 @@ import (
 )
 
 type Task struct {
-	Condition bool            `json:"condition"`
-	Src       string          `json:"src"`
-	Dst       string          `json:"dst"`
-	Content   string          `json:"content"`
-	FileMode  os.FileMode     `json:"file_mode"`
-	DirMode   os.FileMode     `json:"dir_mode"`
-	Config    *configs.Config `json:"config"`
+	Condition bool            `json:"condition" hcl:"condition,optional"`
+	Src       string          `json:"src"       hcl:"src,optional"`
+	Dst       string          `json:"dst"       hcl:"dst"`
+	Content   string          `json:"content"   hcl:"content,optional"`
+	FileMode  os.FileMode     `json:"file_mode" hcl:"file_mode,optional"`
+	DirMode   os.FileMode     `json:"dir_mode"  hcl:"dir_mode,optional"`
+	Config    *configs.Config `json:"config"    hcl:"-"`
 	value     cty.Value
 }
