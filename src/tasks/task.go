5ee0d408c13d5db1e810ba9337ed3b7b611a0ced
Hans Jerry Illikainen <hji@dyntopia.com>
2025-07-06 06:19:05 +0000

    tasks: add qvm_service

--- src/tasks/task.go
+++ src/tasks/task.go
@@ -20,6 +20,7 @@ import (
 	_ "github.com/illikainen/orch/src/tasks/patch"                 // decoder
 	_ "github.com/illikainen/orch/src/tasks/qvm_create"            // decoder
 	_ "github.com/illikainen/orch/src/tasks/qvm_prefs"             // decoder
+	_ "github.com/illikainen/orch/src/tasks/qvm_service"           // decoder
 	_ "github.com/illikainen/orch/src/tasks/symlink"               // decoder
 	_ "github.com/illikainen/orch/src/tasks/systemd"               // decoder
 	_ "github.com/illikainen/orch/src/tasks/systemd_daemon_reload" // decoder
