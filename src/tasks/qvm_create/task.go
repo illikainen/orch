a691c6094cb629026fad6a4bb2b0e9ea641dae15
Hans Jerry Illikainen <hji@dyntopia.com>
2025-07-06 06:55:18 +0000

    tasks: qvm_create: make `prefs` optional

--- src/tasks/qvm_create/task.go
+++ src/tasks/qvm_create/task.go
@@ -12,7 +12,7 @@ type Task struct {
 	Condition   bool              `json:"condition" hcl:"condition,optional"`
 	Name        string            `json:"name"      hcl:"name"`
 	Label       string            `json:"label"     hcl:"label"`
-	Preferences qubes.Preferences `json:"prefs"     hcl:"prefs"`
+	Preferences qubes.Preferences `json:"prefs"     hcl:"prefs,optional"`
 	Services    qubes.Services    `json:"services"  hcl:"services,optional"`
 	Config      *configs.Config   `json:"config"    hcl:"-"`
 	value       cty.Value
